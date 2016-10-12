d6e0d9eb5bf2a163eb7ebb98f8ac9319f589ff2b
Andrzej Szary <aszary@gmail.com>
2016-10-12 16:13:49 +0200

    parser added

--- spa.py
+++ spa.py
@@ -2,17 +2,22 @@
 __author__ = 'Andrzej Szary and Lucas Hermann Negri (peakutils)'
 import os
 import glob
+import argparse
 
 class SinglePulseAnalysis:
 
-    def __init__(self, data_dir="data"):
+    def __init__(self, data_dir):
         self.data_dir = data_dir
 
 
 
 def main():
-    s = SinglePulseAnalysis()
+    parser = argparse.ArgumentParser(prog='spa.py', description=u'Program for pulsar single-pulse analysis', add_help=True)
+    parser.add_argument('-d', '--data', metavar='D', type=str, nargs='?', default='data', help=u'sets the data dir')
 
+    args = parser.parse_args()
+
+    s = SinglePulseAnalysis(data_dir=args.data)
     print "Bye"
 
 
