790d6e90ffc7b049ff5b967e2c90cd2dcb6d722e
Andrzej Szary <aszary@gmail.com>
2016-10-26 08:34:42 +0200

    file names for PSR B0943+10 added

--- spa.py
+++ spa.py
@@ -40,11 +40,14 @@ class SinglePulseAnalysis:
 def main():
     #s = SinglePulseAnalysis(data_dir='/data/szary/J0815+0939/data/')
     #s.load('sJ0815+0939.54015ap', end=None, type='westerbork4')
+    #s2.load('test.dat', end=1000, type='psrchive')  # TODO not implemented yet
     #s.plot()
 
-    s2 = SinglePulseAnalysis(data_dir='/data/szary/B0943/radio/')
-    s2.load('test.asc', end=1000, type='westerbork')
-    #s2.load('test.dat', end=1000, type='psrchive')
+    files = ['./20111105/B0943+10_L33341_RSP0.PrepsubbZerodmNoclip.1_DM15.31.puma.119.gg.1pol.asc', './20111107/B0943+10_L33339_RSP0.PrepsubbNoclip.1_DM15.31.puma.119.gg.1pol.asc', './20111127/B0943+10_L35621_RSP0.PrepsubbZerodmNoclip_DM15.31.puma.119.gg.1pol.asc','./20111201/B0943+10_L36159_RSP0.PrepsubbZerodmNoclip.1_DM15.31.puma.119.gg.1pol.asc','./20111204/B0943+10_L36157_RSP0.PrepsubbZerodmNoclip.1_DM15.31.puma.119.gg.1pol.asc','./20111221/B0943+10_L39707_RSP0.ZerodmNoclip.1_DM15.31.puma.119.gg.1pol.asc','./20120111/B0943+10_L42350_RSP0.PrepdataNoclip.1.puma.119.gg.1pol.asc']
+
+
+    s2 = SinglePulseAnalysis(data_dir='/data/leeuwen/drifting/0943+10/')
+    s2.load(files[0], end=1000, type='westerbork')
     s2.plot()
 
 
